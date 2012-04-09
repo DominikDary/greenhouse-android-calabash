749e98a16d1b9bbe788ece77d74e1c122ad683ff
Dary <ddary@ebay.com>
2012-04-09 21:54:09 +0200

    Added tests and bugfix for setText command for webviews

---
+++ features/step_definitions/ddary_steps.rb
@@ -0,0 +1,11 @@
+Then /^I enter "([^\"]*)" into imput field with id "([^\"]*)"$/ do |text, css|
+  performAction('set_text',text, css)
+end
+
+Then /^I press the button with id "([^\"]*)"$/ do | css|
+  performAction('click_by_selector',css)
+end
+
+Then /^show the html source code$/ do
+  performAction('dump_body_html')
+end
